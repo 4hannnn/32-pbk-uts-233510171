9e832c90fb9b2c2fc370b58747708bfe1eafde3c
4hannnn <genkogt222@gmail.com>
2025-05-09 15:21:12 +0700

    Menambahkan kegiatan baru

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
   <div class="container">
     <h1>Manajemen Kegiatan Saya</h1>
 
+    <div class="add-task">
+      <input type="text" v-model="newTask" placeholder="Apa yang ingin Anda lakukan?" @keyup.enter="addTask" />
+      <button @click="addTask">Tambah</button>
+    </div>
+
     <ul>
       <li v-for="task in tasks" :key="task.id">
         {{ task.text }}
@@ -15,11 +20,24 @@
 export default {
   data() {
     return {
-      tasks: [
-        { id: 1, text: 'Contoh Kegiatan' }
-      ]
+      newTask: '',
+      tasks: []
+    }
+  },
+  methods: {
+    generateId() {
+      return Date.now().toString(36) + Math.random().toString(36).substr(2)
+    },
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.unshift({
+          id: this.generateId(),
+          text: this.newTask.trim(),
+          completed: false
+        })
+        this.newTask = ''
+      }
     }
   }
 }
 </script>
-
